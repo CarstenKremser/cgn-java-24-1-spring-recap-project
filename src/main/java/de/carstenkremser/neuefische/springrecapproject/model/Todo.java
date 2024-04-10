c514c991aa0140168e99aa71aaa4e2c29deb45d5
Carsten Kremser <github@carstenkremser.de>
2024-04-10 15:58:09 +0200

    Add @Document annotation to Todo record

--- src/main/java/de/carstenkremser/neuefische/springrecapproject/model/Todo.java
+++ src/main/java/de/carstenkremser/neuefische/springrecapproject/model/Todo.java
@@ -1,7 +1,9 @@
 package de.carstenkremser.neuefische.springrecapproject.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document("Todo")
 public record Todo(
         @Id
         Integer id,
