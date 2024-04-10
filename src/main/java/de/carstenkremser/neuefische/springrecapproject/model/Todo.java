f5059e93e1717ceecc81f85d339faaa772692f6b
Carsten Kremser <github@carstenkremser.de>
2024-04-10 15:50:06 +0200

    Add @Id annotation to Todo record

--- src/main/java/de/carstenkremser/neuefische/springrecapproject/model/Todo.java
+++ src/main/java/de/carstenkremser/neuefische/springrecapproject/model/Todo.java
@@ -1,6 +1,9 @@
 package de.carstenkremser.neuefische.springrecapproject.model;
 
+import org.springframework.data.annotation.Id;
+
 public record Todo(
+        @Id
         Integer id,
         String description,
         String status
