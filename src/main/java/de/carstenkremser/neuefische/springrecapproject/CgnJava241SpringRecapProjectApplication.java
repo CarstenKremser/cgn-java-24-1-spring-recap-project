4b67cd4ffe1cb72adbafab1209909e587f79d316
Carsten Kremser <github@carstenkremser.de>
2024-04-10 13:39:51 +0200

    Initial commit

---
+++ src/main/java/de/carstenkremser/neuefische/springrecapproject/CgnJava241SpringRecapProjectApplication.java
@@ -0,0 +1,13 @@
+package de.carstenkremser.neuefische.springrecapproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgnJava241SpringRecapProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgnJava241SpringRecapProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/carstenkremser/neuefische/springrecapproject/CgnJava241SpringRecapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.carstenkremser.neuefische.springrecapproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgnJava241SpringRecapProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
